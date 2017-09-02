2d5a9d6a3395d9d40cad038854ab902182706181
Gustavo Borba <gustavohsborba@gmail.com>
2017-09-02 19:47:20 -0300

    Pequena otimização em borba-naive.c e separação em pastas mais organizadas

--- TP/src/borba-naive.c
+++ TP/src/borba-naive.c
@@ -3,19 +3,23 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
- * On exit, A and B maintain their input values. */    
+ * On exit, A and B maintain their input values. */
 void square_dgemm (int n, double* restrict A, double* restrict B, double* restrict C)
 {
   /* For each row i of A */
   for (int i = 0; i < n; ++i)
     /* For each column j of B */
-    for (int j = 0; j < n; ++j) 
+    for (int j = 0; j < n; ++j)
     {
       /* Compute C(i,j) */
       int jn = j*n;
+      int i_jn = i+jn;
       double cij = C[i+jn];
-      for( int k = 0; k < n; k++ )
-        cij += A[i+k*n] * B[k+jn];
+      for( int k = 0; k < n; k++ ){
+        double Aikn=A[i+k*n];
+        double Bkjn=B[k+jn];
+        cij += Aikn * Bkjn;
+      }
       C[i+jn] = cij;
     }
 }
